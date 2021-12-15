23a12a37d93030d62d1df1e925c09b6cf4e1f264
pranavpatel <pranavpatel986@gmail.com>
2021-12-16 02:06:58 +0530

    Update MatchFile.java

--- src/fileHandlingAssignment/MatchFile.java
+++ src/fileHandlingAssignment/MatchFile.java
@@ -2,27 +2,44 @@ package fileHandlingAssignment;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 
 public class MatchFile {
-
+	public static void main(String[] args) {
+		match_File();
+	}
     public static void match_File(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter some String : ");
-        String temp = sc.next();
-//        temp.toUpperCase();
-        String temp2 = temp.toUpperCase();
+        String substring1 = sc.next();
+        String substring = substring1.toUpperCase();
         File f = new File("D:\\main");
-        File[] matchingFiles = f.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                if (name.toUpperCase().startsWith(temp2)){
-                    System.out.println(name);
-                }
-                return name.startsWith(temp2);
+        
+        List<String> filesContainingSubstring = new ArrayList<String>();
+
+        if( f.exists() && f.isDirectory() )
+        {
+            String[] files = f.list(); //get the files in String format.
+            for( String fileName : files )
+            {
+            	
+                if( fileName.toUpperCase().contains( substring ) ) 
+                     filesContainingSubstring.add( fileName );
             }
-            
-        });
+        }
+
+        for( String fileName : filesContainingSubstring )
+        {
+           System.out.println( fileName ); //or do other operation 
+        }
+     }
        
-    }
-    
 }
+    
+
+
+
+
